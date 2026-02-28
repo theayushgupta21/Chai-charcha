50f9b69f09782e3f245f9744da187e74637f2bc7
Ayush Gupta <ayushgupta9510@gmail.com>
2026-02-28 14:25:52 +0530

    new added

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className=" bg-amber-500 text-amber-950 mt-10 p-5 rounded-lg pt-3.5 h-11 w-32 text-center text-sm font-medium items-center justify-center flex hover:bg-amber-600 cursor-pointer ">
+        hey
+      </div>
+    </>
+
+  )
+}
+
+export default App
